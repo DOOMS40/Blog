1171e80113b4cde8effb503de1b2c559436718e3
Kostya_Kobel <kostp62020@gmail.com>
2021-01-30 18:37:36 +0200

    change routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'users/show'
-  root to: "home#index"
+  root to: "users#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
