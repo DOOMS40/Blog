44824837387272f7eb9a40114d5dc6f35885d7a7
Kostya_Kobel <kostp62020@gmail.com>
2021-01-31 16:36:15 +0200

    add users/user_images routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
   root to: "users#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users do
+    resources :user_images
+  end
 end
