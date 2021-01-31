7a26e7685aa19baca3e8a683cc9029b8685cc845
Kostya_Kobel <kostp62020@gmail.com>
2021-01-31 16:34:35 +0200

    adding links between models by user and user_image

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   mount_uploader :image, ImageUploader
+  has_many :user_images
 end
