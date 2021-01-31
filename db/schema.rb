01cbe6381a4521ae0b97b225d78abf18cbec99d2
Kostya_Kobel <kostp62020@gmail.com>
2021-01-31 16:49:24 +0200

    migration carried out for user_images

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210131114946) do
+ActiveRecord::Schema.define(version: 20210131141710) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -36,6 +36,15 @@ ActiveRecord::Schema.define(version: 20210131114946) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "user_images", force: :cascade do |t|
+    t.text     "description"
+    t.string   "image"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["user_id"], name: "index_user_images_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
