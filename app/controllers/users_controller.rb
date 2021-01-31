5dfad29c0e7f7c78e9a6e82270bd1e938f142476
Kostya_Kobel <kostp62020@gmail.com>
2021-01-31 13:37:47 +0200

    creating a private method for secure data transmission

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   def show
+    user_find
+  end
+
+  private 
+  
+  def user_find
     @user = User.find(1)
   end
 end
