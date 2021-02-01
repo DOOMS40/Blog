d5f3732094c7f368fcc499d35481d6eaca2c9043
Kostya_Kobel <kostp62020@gmail.com>
2021-02-01 17:39:11 +0200

    add functional for show,new,create methods

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,30 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
   def show
+    comment_find
   end
 
   def new
+    @comment = Comment.new
   end
 
   def create
+    @user = User.find(params[:user_id])
+    @comment = @user.comments.new(comment_params)
+    if @comment.save
+      redirect_to @user
+    else 
+      render :new
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:authors,:description)
+  end
+
+  def comment_find
+    @comment = Comment.find(params[:id])
   end
 end
