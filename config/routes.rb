0c9b597403999ce1e0cd485adcfde7a10130204a
Kostya_Kobel <kostp62020@gmail.com>
2021-02-01 14:57:07 +0200

    config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'searches/search'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
   root to: "users#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     resources :user_images
+
+    resources :searches
 end
