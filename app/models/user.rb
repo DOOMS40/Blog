ed89e1a764f8c20fc39d82ca6a35ad75b3037a57
Kostya_Kobel <kostp62020@gmail.com>
2021-02-01 14:54:00 +0200

    adding a link between the search and user models

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   mount_uploader :image, ImageUploader
   has_many :user_images
+  has_many :friendships
+  has_many :searches
 end
